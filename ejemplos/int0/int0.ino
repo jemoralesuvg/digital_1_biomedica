a75ae99904259593f9827ea72c41f601abf3ee73
wjosemorales <wjosemorales@gmail.com>
2023-04-26 16:52:08 -0600

    int0 example

---
+++ ejemplos/int0/int0.ino
@@ -0,0 +1,20 @@
+void setup()
+{
+  DDRD &= ~(1 << PD2);   // Configura PD2 como entrada (PIN3)
+  PORTD |= (1 << PD2);   // Habilita la resistencia de pull-up interna en PD2
+  EICRA |= (1 << ISC01); // INT0 para que se active en flanco de bajada
+  EIMSK |= (1 << INT0);  // Habilita la interrupción externa INT0
+
+  Serial.begin(9600);
+}
+// Función de interrupción para INT0
+ISR(INT0_vect) {
+  Serial.println ("interrupción");
+  // Código que se ejecutará cuando se active la interrupción
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

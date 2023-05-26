611b82bed4a876b45c7b7166b52fea20b8cf51a9
Jose Morales <49993116+jemoralesuvg@users.noreply.github.com>
2023-05-26 14:51:33 -0600

    Update multiple_pwm.ino

--- ejemplos/multiple_pwm/multiple_pwm.ino
+++ ejemplos/multiple_pwm/multiple_pwm.ino
@@ -1,11 +1,10 @@
 void setup() {
   // put your setup code here, to run once:
-  // Configurar los pines OC1A a OC1F como salidas
-  DDRB |= (1 << DDB5) | (1 << DDB6) | (1 << DDB7);
-  DDRD |= (1 << DDD4) | (1 << DDD5) | (1 << DDD6);
+  // Configurar los pines 9 y 10 como salidas
+  DDRB |= (1 << DDB1) | (1 << DDB2) ;
 
   // Configurar el temporizador en modo Fast PWM de 10 bits
-  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << COM1C1) | (1 << WGM11) | (1 << WGM10);
+  TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11) | (1 << WGM10);
   TCCR1B = (1 << WGM13) | (1 << WGM12) | (1 << CS11); // Divisor de reloj en 8
 
   // Configurar el valor máximo del contador para PWM de 10 bits
@@ -14,10 +13,6 @@ void setup() {
   // Configurar los valores de comparación para cada salida PWM
   OCR1A = 200;
   OCR1B = 400;
-  OCR1C = 600;
-  OCR1D = 800;
-  OCR1E = 900;
-  OCR1F = 1000;
 
 }
 
